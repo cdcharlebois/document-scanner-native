c9644ba95d03545b8b85b6b57f5a7be6af98fbca
Conner Charlebois <cdcharlebois@gmail.com>
2022-06-14 15:00:42 -0400

    show the rectangle overlay (only works on fullscreen layouts for now)

--- src/DocumentScanner.jsx
+++ src/DocumentScanner.jsx
@@ -1,15 +1,13 @@
-import { Component, createElement, useRef } from "react";
+import { Component, createElement, useState, useRef } from "react";
 import { View } from "react-native";
-import Scanner from "react-native-rectangle-scanner";
+import Scanner, { RectangleOverlay } from "react-native-rectangle-scanner";
 import PropTypes from "prop-types";
 
-// import { HelloWorld } from "./components/HelloWorld";
-
 export const DocumentScanner = () => {
-    console.info("rendered");
-    console.info("rendered");
+    
     console.log(PropTypes);
-
+    const [rectangleDetected, setRectangleDetected] = useState(null);
+    const boundary = useRef();
     // const camera = useRef();
 
     const handlePictureTaken = data => {
@@ -17,19 +15,47 @@ export const DocumentScanner = () => {
         console.log(data);
     };
 
-    const handleRectangleDetected = ({detectedRectangle}) => {
+    const handleRectangleDetected = ({ detectedRectangle }) => {
         if (!detectedRectangle) return;
         console.log("rectangle detected");
         console.log(detectedRectangle);
-    }
+        boundary.current.measure((fx, fy, w, h, px, py)=>{
+            console.log("py", py);
+            console.log("fy", fy);
+            setRectangleDetected(detectedRectangle);
+        });
+        
+    };
 
-    // const onCapture = () => {
-    //     camera.current.capture();
-    // }
+    const renderRectangleOverlay = () => {
+        if (!rectangleDetected) return null;
 
-    return ( 
-        <View style={{flex:1}}>
-            <Scanner onPictureProcessed={handlePictureTaken} style={{flex: 1}} onRectangleDetected={handleRectangleDetected} />
+        // {
+        //     "topRight": { "y": 1330.0572681427002, "x": 133.67738470435143 },
+        //     "topLeft": { "y": 1168.6467218399048, "x": -5.12178341858089 },
+        //     "dimensions": { "width": 910, "height": 1560 },
+        //     "bottomLeft": { "y": 998.9343523979187, "x": 164.8810277879238 },
+        //     "bottomRight": { "y": 1205.8252358436584, "x": 288.4637638926506 }
+        //   }
+          
+        return (
+            <RectangleOverlay
+                detectedRectangle={rectangleDetected}
+                backgroundColor="rgba(255,181,6,0.2)"
+                borderColor="tomato"
+                borderWidth="4"
+            />
+        );
+    };
+
+    return (
+        <View style={{ flex: 1, backgroundColor:"rgba(0,0,0,0)", position:"relative" }} ref={boundary}>
+            <Scanner
+                onPictureProcessed={handlePictureTaken}
+                style={{ flex: 1 }}
+                onRectangleDetected={handleRectangleDetected}
+            />
+            {renderRectangleOverlay()}
         </View>
     );
 };
