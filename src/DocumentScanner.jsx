b98327b905aa5bf2b120bab099fa9046bcd73710
Conner Charlebois <cdcharlebois@gmail.com>
2022-06-14 13:57:52 -0400

    recognize when a rectangle is detected

--- src/DocumentScanner.jsx
+++ src/DocumentScanner.jsx
@@ -1,4 +1,4 @@
-import { Component, createElement } from "react";
+import { Component, createElement, useRef } from "react";
 import { View } from "react-native";
 import Scanner from "react-native-rectangle-scanner";
 import PropTypes from "prop-types";
@@ -6,14 +6,30 @@ import PropTypes from "prop-types";
 // import { HelloWorld } from "./components/HelloWorld";
 
 export const DocumentScanner = () => {
+    console.info("rendered");
+    console.info("rendered");
     console.log(PropTypes);
+
+    // const camera = useRef();
+
     const handlePictureTaken = data => {
+        console.log("picture taken");
         console.log(data);
     };
 
-    return (
-        <View>
-            <Scanner onPictureProcessed={handlePictureTaken} style={{flex: 1}} />
+    const handleRectangleDetected = ({detectedRectangle}) => {
+        if (!detectedRectangle) return;
+        console.log("rectangle detected");
+        console.log(detectedRectangle);
+    }
+
+    // const onCapture = () => {
+    //     camera.current.capture();
+    // }
+
+    return ( 
+        <View style={{flex:1}}>
+            <Scanner onPictureProcessed={handlePictureTaken} style={{flex: 1}} onRectangleDetected={handleRectangleDetected} />
         </View>
     );
 };
